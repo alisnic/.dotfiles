94d277db43dfb2adc7b6d7e4dda122fe38544371
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-03-21 16:04:39 +0000

    Remove init.sh since it is useless

--- init.sh
+++
@@ -1,9 +0,0 @@
-set -e
-
-cd
-ln -s .dotfiles/.zshrc .zshrc
-ln -s .dotfiles/.vim .vim
-ln -s .dotfiles/.gemrc .gemrc
-ln -s .dotfiles/.vimrc .vimrc
-ln -s .dotfiles/.gitconfig .gitconfig
-ln -s .dotfiles/.tmux.conf .tmux.conf
