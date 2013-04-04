4616ec4e0472fb4fd1998fa690ba97ff5e0ce969
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-04-04 11:19:44 +0300

    Increase topbar height

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -180,7 +180,7 @@ for s = 1, screen.count() do
     mytasklist[s] = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, mytasklist.buttons)
 
     -- Create the wibox
-    mywibox[s] = awful.wibox({ position = "top", screen = s })
+    mywibox[s] = awful.wibox({ position = "top", screen = s, height = "24"})
 
     -- Widgets that are aligned to the left
     local left_layout = wibox.layout.fixed.horizontal()
