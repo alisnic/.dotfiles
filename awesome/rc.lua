b8fdc36383a8654e59fbbc1bb9e994ed640b9f57
Andrei Lisnic <andrei.lisnic@gmail.com>
2013-04-09 12:20:12 +0300

    Name the third desktop

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -82,7 +82,7 @@ end
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
 tags = {}
-tag_names = {"1 (www+term)", "2 (skype)", 3, 4, 5}
+tag_names = {"1 (www+term)", "2 (skype)", "3 (files+dl)", 4, 5}
 for s = 1, screen.count() do
     -- Each screen has its own tag table.
     tags[s] = awful.tag(tag_names, s, awful.layout.suit.max)
