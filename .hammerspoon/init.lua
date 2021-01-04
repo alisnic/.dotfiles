5f2057a4a0e8f1f9c0e72deaf447184fbb757a72
Andrei Lisnic <andrei.lisnic@gmail.com>
2021-01-04 09:58:28 +0200

    Switch to Mail.app

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -16,7 +16,7 @@ end
 
 hs.hotkey.bind({"alt"}, "c", function() focusApp("Google Chrome") end)
 hs.hotkey.bind({"alt"}, "x", function() focusApp("Mattermost") end)
-hs.hotkey.bind({"alt"}, "m", function() focusApp("Thunderbird") end)
+hs.hotkey.bind({"alt"}, "m", function() focusApp("Mail") end)
 hs.hotkey.bind({"alt"}, "§", function() focusApp("iTerm") end)
 hs.hotkey.bind({"alt"}, "p", function() focusApp("MacPass") end)
 hs.hotkey.bind({"alt"}, "f", function() focusApp("Finder") end)
