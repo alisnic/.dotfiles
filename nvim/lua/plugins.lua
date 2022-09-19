01901a1a7f2aafb9db8ae75269a3814125d4db41
Andrei Lisnic <andrei@planable.io>
2022-09-19 22:12:09 +0300

    Try treesitter context

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -191,6 +191,13 @@ require("packer").startup(function(use)
   }
 
   use {
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup()
+    end,
+  }
+
+  use {
     "lewis6991/spellsitter.nvim",
     config = function()
       require("spellsitter").setup()
@@ -320,8 +327,7 @@ end)
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0
-    and vim.api
-        .nvim_buf_get_lines(0, line - 1, line, true)[1]
+    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
         :sub(col, col)
         :match "%s"
       == nil
