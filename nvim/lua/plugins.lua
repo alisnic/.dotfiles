f1cf97a6ae52eb523947da31779404516f009a39
Andrei Lisnic <andrei@planable.io>
2022-11-17 11:32:42 +0200

    don't override cmp stuff cause it's better

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -57,7 +57,7 @@ require("packer").startup(function(use)
           override = {
             ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
             ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
+            ["cmp.entry.get_documentation"] = false,
           },
         },
         presets = {
@@ -65,7 +65,7 @@ require("packer").startup(function(use)
           command_palette = false,
           long_message_to_split = true,
           inc_rename = false,
-          lsp_doc_border = true,
+          lsp_doc_border = false,
         },
       }
     end,
