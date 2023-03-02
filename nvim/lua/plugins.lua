ef8a50512ab8cfad826147f3b2f36e10595816c9
Andrei Lisnic <andrei@planable.io>
2023-03-02 15:39:49 +0200

    Cleanup

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -62,11 +62,7 @@ require("packer").startup(function(use)
           },
         },
         presets = {
-          bottom_search = true,
-          command_palette = true,
-          long_message_to_split = true,
-          inc_rename = false,
-          lsp_doc_border = true,
+          lsp_doc_border = true
         },
       }
     end,
