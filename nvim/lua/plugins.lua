892f5d7bca4125626b75bd5d75db96eb888bc994
Andrei Lisnic <andrei@planable.io>
2023-11-06 11:20:38 +0200

    Revert gruvbox fork

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -265,9 +265,7 @@ require("packer").startup(function(use)
   }
 
   use {
-    -- "ellisonleao/gruvbox.nvim",
-    "chuck-flowers/gruvbox.nvim",
-    branch = "neogit",
+    "ellisonleao/gruvbox.nvim",
     config = function()
       require("gruvbox").setup {
         bold = false,
@@ -494,8 +492,7 @@ end)
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0
-    and vim.api
-        .nvim_buf_get_lines(0, line - 1, line, true)[1]
+    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]
         :sub(col, col)
         :match "%s"
       == nil
