12f79278df4859fbc2b637685c7a14e83050e440
Andrei Lisnic <andrei@planable.io>
2024-03-07 11:25:56 +0200

    Bring back dressing

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -31,6 +31,7 @@ require("packer").startup(function(use)
   use "kchmck/vim-coffee-script"
   use "folke/neodev.nvim"
   use "michaeljsmith/vim-indent-object"
+  use "stevearc/dressing.nvim"
   use {
     "seblj/nvim-tabline",
     requires = { "nvim-tree/nvim-web-devicons" },
