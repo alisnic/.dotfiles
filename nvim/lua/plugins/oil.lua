70546afcbedcf11eee6b185957a1b1b4fc5321ae
Andrei Lisnic <andrei@planable.io>
2026-05-08 15:25:37 +0300

    oil reveal

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -12,6 +12,22 @@ function M.setup()
       timeout_ms = 1000,
       autosave_changes = "unmodified",
     },
+    keymaps = {
+      ["gR"] = {
+        callback = function()
+          local oil = require("oil")
+          local dir = oil.get_current_dir()
+          local entry = oil.get_cursor_entry()
+
+          if not dir or not entry then
+            return
+          end
+
+          vim.system({ "open", "-R", dir .. entry.name }, { detach = true })
+        end,
+        desc = "Reveal in Finder",
+      },
+    },
   })
 
   vim.keymap.set("n", "-", "<cmd>Oil<cr>", { silent = true, desc = "Open Parent Directory" })
